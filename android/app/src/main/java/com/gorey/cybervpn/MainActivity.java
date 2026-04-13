c82b7983186a719f27f745551d1e175dc2bbfc42
pinkth3floyd <prakash@zeptosky.com>
2026-04-13 13:40:24 +0545

    android init

---
+++ android/app/src/main/java/com/gorey/cybervpn/MainActivity.java
@@ -0,0 +1,5 @@
+package com.gorey.cybervpn;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.gorey.cybervpn',
+  appName: 'cyber-vpn',
+  webDir: 'dist'
+  
+};
+
+export default config;
